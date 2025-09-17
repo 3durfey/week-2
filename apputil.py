19d4a2cfed31987d1bf4a693d0e517a812aff321
Peter Durfey <56773327+3durfey@users.noreply.github.com>
2025-09-17 01:48:08 +0000

    test

--- apputil.py
+++ apputil.py
@@ -4,10 +4,6 @@ import numpy as np
 # update/add code below ...
 
 def ways(n):
-    return None
+    return "n"
 
-def lowest_score(names, scores):
-    return None
-
-def sort_names(names, scores):
-    return None
+def lowest_score(names, scores):fsdadfsd
